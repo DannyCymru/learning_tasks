dc077ace6bc208f1525ed3f1349d65b6c30adab5
Danny Roberts <danny@erebus.cymru>
2020-07-18 17:20:50 +0100

    Messed around with functions and ranged loops in rust

--- Rust/hello_cargo/src/main.rs
+++ Rust/hello_cargo/src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate text_io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello. Cargo? Please enter a number you would like to count to: ");
+    let num: i32 = read!();
+    count(num);
+}
+
+fn count(num:i32){
+
+	for n in 1..=num {
+		println!("{}", n);
+	}
 }
